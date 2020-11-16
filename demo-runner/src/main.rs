7d2b904e1483a89c7d3030416c10cf5193d45ea1
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-11-16 10:16:18 +0700

    Remove compiler side-effects on iterative

--- demo-runner/src/main.rs
+++ demo-runner/src/main.rs
@@ -1,7 +1,16 @@
+use clap::Clap;
 use demo_fibonacci::{fibonacci_iterative, fibonacci_recursive, fibonacci_recursive_cached};
 use xshell::cmd;
 
-const FN: u128 = 44;
+#[derive(Clap)]
+#[clap(
+    version = "0.1.0",
+    author = "Aditya Kresna <aditya.kresna@outlook.co.id>"
+)]
+struct Opts {
+    #[clap(short, long)]
+    fibo_n: u128,
+}
 
 macro_rules! time_it {
     ($context:literal, $($tt:tt)+) => {
@@ -14,6 +23,8 @@ macro_rules! time_it {
 }
 
 fn main() {
+    let opts = Opts::parse();
+
     let result_fr;
     let result_fi;
     let result_frc;
@@ -22,25 +33,25 @@ fn main() {
 
     println!("---");
     time_it!("[Node] Recursive Time", {
-        let fn_str = FN.to_string();
+        let fn_str = opts.fibo_n.to_string();
         let str_result = cmd!("node fibo_recursive.js {fn_str}").read().unwrap();
         result_fr_js = u128::from_str_radix(&str_result, 10).unwrap();
     });
     time_it!("[Rust] Recursive Time", {
-        result_fr = fibonacci_recursive(FN);
+        result_fr = fibonacci_recursive(opts.fibo_n);
     });
     time_it!("[Rust] Recursive Cached Time", {
-        result_frc = fibonacci_recursive_cached(FN);
+        result_frc = fibonacci_recursive_cached(opts.fibo_n);
     });
 
     println!("---");
     time_it!("[Node] Iterative Time", {
-        let fn_str = FN.to_string();
+        let fn_str = opts.fibo_n.to_string();
         let str_result = cmd!("node fibo_iterative.js {fn_str}").read().unwrap();
         result_fi_js = u128::from_str_radix(&str_result, 10).unwrap();
     });
     time_it!("[Rust] Iterative Time", {
-        result_fi = fibonacci_iterative(FN);
+        result_fi = fibonacci_iterative(opts.fibo_n);
     });
 
     assert!(
@@ -51,5 +62,5 @@ fn main() {
     );
 
     println!("---");
-    println!("Fibo({}): {}", FN, result_fi);
+    println!("Fibo({}): {}", opts.fibo_n, result_fi);
 }
